c91a223ffa728807691f00ece2bdcdf054cd831a
Sergey <sega00796@mail.ru>
2018-03-28 20:47:44 +0300

    контрольная

--- pics.py
+++ pics.py
@@ -1,8 +1,14 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import math
 
-step = 100
+#config me here#
+method = 2
+################
+################
+
+#step = 100
 
 color_white = (255,255,255)
 color_black = (0,0,0)
@@ -64,24 +70,52 @@ cv2.fillPoly(image, a3,color_gray)
 #cv2.rectangle(image,(200,0),(200,100),color_black,-1)
 #cv2.rectangle(image,(200,100),(200,300),color_black,-1)
 
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
 plt.imshow(image)
 plt.show()
 
-#image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 #plt.imshow(image)
 #plt.show()
 
 #doing
-kernel1 = np.array([[-1, 0,1],[-1,0,1],[-1,0,1]])
-dst1 = cv2.filter2D(image, -1, kernel1)
 
-kernel2 = np.array([[-1, -1,-1],[0,0,0],[1,1,1]])
-dst2 = cv2.filter2D(image, -1, kernel2)
+if method == 0:
+    kernel1 = np.array([[-1/9, 0,1/9],[-1/9,0,1/9],[-1/9,0,1/9]])
+    dst1 = cv2.filter2D(image, -1, kernel1)
 
-plt.imshow(dst1)
+    kernel2 = np.array([[-1/9, -1/9,-1/9],[0,0,0],[1/9,1/9,1/9]])
+    dst2 = cv2.filter2D(image, -1, kernel2)
+elif method == 1:
+    kernel1 = np.array([[-1, 0,1],[-1,0,1],[-1,0,1]])
+    dst1 = cv2.filter2D(image, -1, kernel1)
+
+    kernel2 = np.array([[-1, -1,-1],[0,0,0],[1,1,1]])
+    dst2 = cv2.filter2D(image, -1, kernel2)
+elif method == 2:
+    dst1 = cv2.Sobel(image,cv2.CV_64F,1,0,ksize=3)
+    dst2 = cv2.Sobel(image,cv2.CV_64F,0,1,ksize=3)
+
+
+cv2.imshow(dst1, cmap = 'gray')
+plt.show()
+#cv2.waitKey(0)
+
+plt.imshow(dst2, cmap = 'gray')
 plt.show()
 
-plt.imshow(dst2)
+theNewImage = np.zeros((600,300,3), dtype=np.uint8)
+
+for i in range(len(dst1)):
+    for j in range(len(dst1[i])):
+        theNewImage[i][j][0] = math.sqrt(dst1[i][j] ** 2 + dst2[i][j] ** 2)
+        theNewImage[i][j][1] = dst1[i][j]
+        theNewImage[i][j][2] = dst2[i][j]
+
+plt.imshow(theNewImage)
 plt.show()
+
+
+
 
 #new pic
